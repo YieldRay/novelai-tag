53a27f7ac8ee2956102c331f18fb0f95539ac192
YieldRay <yieldray@outlook.com>
2022-10-28 11:34:41 +0800

    basic support for pwa

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,37 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 import sveltePreprocess from "svelte-preprocess";
+import pkg from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         svelte({
             preprocess: sveltePreprocess(),
+        }),
+        VitePWA({
+            includeAssets: ["favicon-32x32.png", "favicon-16x16.png", "apple-touch-icon.png", "safari-pinned-tab.svg"],
+            manifest: {
+                name: pkg.description,
+                short_name: pkg.name,
+                description: pkg.description,
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+                start_url: "/",
+                registerType: "autoUpdate",
+            },
         }),
     ],
 });
