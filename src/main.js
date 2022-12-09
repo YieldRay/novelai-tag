c9fdbd6a40e5d8e438183e3302b1278e82585f00
YieldRay <yieldray@outlook.com>
2022-12-09 20:42:01 +0800

    bump up dependencies

--- src/main.js
+++ src/main.js
@@ -6,23 +6,3 @@ const app = new App({
 });
 
 export default app;
-
-
-//? Statistic
-fetch("https://kokomi.deta.dev/record", {
-    method: "POST",
-    headers: { "Content-type": "application/json; charset=utf-8" },
-    body: JSON.stringify({ url: location.href, ref: document.referrer, lang: navigator.language }),
-});
-
-window.onerror = function (msg, src, lineno, colno, error) {
-    fetch("https://kokomi.deta.dev/error", {
-        method: "POST",
-        headers: { "Content-type": "application/json; charset=utf-8" },
-        body: JSON.stringify({
-            url: location.href,
-            src: src,
-            error: { name: error.name, message: error.message, stack: error.stack },
-        }),
-    });
-};
